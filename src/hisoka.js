f7ca37aa581e6843783e4641c8b141411a35ae68
emangparapeopleknow-sudo <emangparapeopleknow@gmail.com>
2026-06-04 15:23:10 +0800

    Update hisoka.js

--- src/hisoka.js
+++ src/hisoka.js
@@ -50,6 +50,7 @@ async function start() {
     },
     puppeteer: {
         headless: true,
+        bypassCSP: false,
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
@@ -61,8 +62,7 @@ async function start() {
         ]
     },
     webVersionCache: {
-        type: 'remote',
-        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html'
+        type: 'local',
     }
 });
 
